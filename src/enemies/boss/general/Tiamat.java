ddce1107cd650aa91e7b6c0e720a87eef86b0e30
GogiRocks <aph@mchsi.com>
2015-02-25 15:48:01 -0600

    Test

---
+++ src/enemies/boss/general/Tiamat.java
@@ -0,0 +1,40 @@
+package enemies.boss.general;
+
+import enemies.boss.Boss;
+import org.lwjgl.opengl.GL11.*;
+
+public class Tiamat implements Boss{
+
+	@Override
+	public void hit(byte damage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void death() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public byte primeAttack() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public byte secondaryAttack() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public byte finishingAttack() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
