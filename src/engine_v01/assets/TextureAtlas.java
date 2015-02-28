31742f7843071313a7a185321f708d19575601fb
shutterbug2000 <shutterbug20002@yahoo.com>
2015-02-28 14:46:57 -0600

    Starting on TextureAtlas

---
+++ src/engine_v01/assets/TextureAtlas.java
@@ -0,0 +1,20 @@
+package engine_v01.assets;
+
+import org.newdawn.slick.opengl.Texture;
+
+public class TextureAtlas extends Animation {
+
+	private Texture texture;
+	private Vec2 dimensions;
+	
+	public TextureAtlas(int length) {
+		super(length);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void texCoord(Vec2 texCoord) {
+		
+	}
+
+}
