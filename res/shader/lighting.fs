a10aae35cbd8a6be6dd8e2b5972d5b7e09ea20af
shutterbug2000 <shutterbug20002@yahoo.com>
2015-02-28 19:21:27 -0600

    Lighting shaders like a bossss

--- res/shader/lighting.fs
+++ res/shader/lighting.fs
@@ -2,11 +2,14 @@ uniform float time;
 uniform sampler2D textureUnit;
 uniform vec2 resolution;
 uniform vec4 color;
+uniform vec2 center;
 
 varying vec4 vertex;
 varying vec2 texCoord;
+uniform vec2 point;
 
 void main() {
-    vec4 color = texture2D(textureUnit, texCoord);
-    gl_FragColor = color;
+	vec4 color = texture2D(textureUnit, texCoord);
+	color /= distance(gl_FragCoord.xy, point) / 100;
+   	gl_FragColor = color;
 }
